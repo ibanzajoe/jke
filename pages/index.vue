b9b48a05875b72effb10163459f686b368fb1b15
Dennis Lee <ibanzajoe@gmail.com>
2019-09-11 02:39:37 -0700

    added new files

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-grid">
+    <div class="grid-hero">
+
+    </div>
+    <div class="grid-services">
+
+    </div>
+    <div class="grid-booking">
+
+    </div>
+    <div class="grid-style">
+
+    </div>
+    <div class="grid-contact">
+
+    </div>
+    <div class="grid-footer">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
